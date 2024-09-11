7501fd88717ad6c961fd1c986e609936a645c3d7
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-09-11 03:15:55 +0000

    added dice roll and error messages

--- dice.rb
+++ dice.rb
@@ -1,4 +1,11 @@
 require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   "Hello World"
@@ -6,4 +13,40 @@ end
 
 get("/zebra") do
   "We must add a route for each path we want to support"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+
+  "<h1>2d10</h1>
+    <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do
+  twenty_die = rand(1..20)
+  sum = twenty_die
+
+  outcome = "You rolled a #{twenty_die}"
+
+  "<h1>d20</h1>
+    <p>#{outcome}</p>"
 end
