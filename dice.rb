1d8fb948b669ef3fa9cd9d8c525b059ef4188fb6
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-09-11 04:08:58 +0000

    two_six finished

--- dice.rb
+++ dice.rb
@@ -2,13 +2,14 @@ require "sinatra"
 require "sinatra/reloader"
 require "better_errors"
 require "binding_of_caller"
+  
 
 use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
- erb(:elephant)
+  erb(:elephant)
 end
 
 get("/zebra") do
@@ -20,34 +21,34 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+    
+    @rolls.push(die)
+	end
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+	erb(:two_six)
 end
 
 get("/dice/2/10") do
   first_die = rand(1..10)
-  second_die = rand(1..10)
+	second_die = rand(1..10)
   sum = first_die + second_die
-
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
-
-  "<h1>2d10</h1>
-    <p>#{outcome}</p>"
+	
+	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  twenty_die = rand(1..20)
-
-  outcome = "You rolled a #{twenty_die}"
-
-  "<h1>d20</h1>
-    <p>#{outcome}</p>"
+  @die = rand(1..20)
+	
+	@outcome = "You rolled a #{@die}."
+	
+	erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -57,8 +58,19 @@ get("/dice/5/4") do
   fourth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die
 	
-	outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
 	
-	"<h1>5d4</h1>
-   <p>#{outcome}</p>"
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []    # Create a blank array
+
+  100.times do    # 100 times...
+    die = rand(1..6)    # Generate a random number
+
+    @rolls.push(die)    # Add the random number to the array 
+  end
+
+  erb(:one_hundred_six)
 end
